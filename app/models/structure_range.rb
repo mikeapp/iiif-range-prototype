497780880d3c80c87785b148436e90c780e66d24
mikeapp <michael.appleby@yale.edu>
2022-07-12 10:08:17 -0400

    Output partOf reference to Parent Manifest

--- app/models/structure_range.rb
+++ app/models/structure_range.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class StructureRange < Structure
+
+  def parent_range
+    return nil unless structure_id
+    StructureRange.find(structure_id)
+  end
+
   def child_structures_as_iiif
     children = []
     structures.order('position').each do |s|
@@ -10,15 +16,25 @@ class StructureRange < Structure
   end
 
   def to_iiif
+    partOf = []
+    if parent_range
+      partOf << {
+        id: IiifRangeBuilder.uuid_to_uri(parent_range.resource_id),
+        type: 'Range'
+      }
+    elsif top_level
+      partOf << {
+        id: IiifRangeBuilder.parent_uri_from_id(parent_object_id),
+        type: 'Manifest'
+      }
+    end
+
     {
       id: IiifRangeBuilder.uuid_to_uri(resource_id),
       type: 'Range',
       label: { "en": [label] },
       items: child_structures_as_iiif,
-      partOf: [{
-        id: IiifRangeBuilder.parent_uri_from_id(parent_object_id),
-        type: 'Manifest'
-      }]
+      partOf: partOf
     }
   end
 end
